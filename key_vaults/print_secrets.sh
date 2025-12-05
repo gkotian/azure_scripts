a9ceb4906fa7c7bfc7e278c004e43774155ff44b
Gautam Kotian <gkotian@users.noreply.github.com>
2025-12-05 10:43:43 +0100

    Remove duplicate entries

--- key_vaults/print_secrets.sh
+++ key_vaults/print_secrets.sh
@@ -30,26 +30,23 @@ fi
 
 IFS=',' read -r -a KEY_VAULTS_ARRAY <<< "${KEY_VAULTS_CSV}"
 IFS=',' read -r -a SECRETS_ARRAY <<< "${SECRETS_CSV}"
+SECRETS_ARRAY=($(printf "%s\n" "${SECRETS_ARRAY[@]}" | xargs -n1 | sort -u)) # Trim whitespace and remove duplicates
+
+RESOLVED_KEY_VAULTS=()
+for KV in "${KEY_VAULTS_ARRAY[@]}"; do
+    KV=$(echo "${KV}" | xargs)   # Trim whitespace
+    [[ -z "${KV}" ]] && continue # Skip empty
+    RESOLVED_KEY_VAULTS+=("$(resolve_key_vault "${KV}")") # Resolve abbreviations
+done
+RESOLVED_KEY_VAULTS=($(printf "%s\n" "${RESOLVED_KEY_VAULTS[@]}" | sort -u)) # Remove duplicates
 
 PAST_SEARCHES_FILE="/var/tmp/print_secrets_past_executions.log"
 
-for KEY_VAULT in "${KEY_VAULTS_ARRAY[@]}"; do
-    KEY_VAULT=$(echo "${KEY_VAULT}" | xargs)  # Trim whitespace
-    if [ -z "${KEY_VAULT}" ]; then
-        continue
-    fi
-
-    KEY_VAULT=$(resolve_key_vault "${KEY_VAULT}")
-
+for KEY_VAULT in "${RESOLVED_KEY_VAULTS[@]}"; do
     echo ""
     echo "${KEY_VAULT}:"
 
     for SECRET_NAME in "${SECRETS_ARRAY[@]}"; do
-        SECRET_NAME=$(echo "${SECRET_NAME}" | xargs)  # Trim whitespace
-        if [ -z "${SECRET_NAME}" ]; then
-            continue
-        fi
-
         SECRET_VALUE=$(az keyvault secret show --vault-name "${KEY_VAULT}" --name "${SECRET_NAME}" 2>/dev/null | jq --raw-output '.value' || echo "<<< NOT FOUND >>>")
         echo "    ${SECRET_NAME}: ${SECRET_VALUE}"
     done
